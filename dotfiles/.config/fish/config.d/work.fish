eeb0a29c19329fde532e602d2ca6621c24f0affb
Joshua Marsh (icub3d) <joshua@themarshians.com>
2020-04-20 22:10:19 -0600

    work

---
+++ dotfiles/.config/fish/config.d/work.fish
@@ -0,0 +1,9 @@
+#!/usr/bin/fish
+
+if test (hostname) = "work"
+   set -x OSE_USER jmars23
+   set -x GIT_AUTHOR_NAME 'Joshua Marsh'
+   set -x GIT_AUTHOR_EMAIL 'josh.marsh@optum.com'
+   set -x GIT_COMMITTER_NAME 'Joshua Marsh'
+   set -x GIT_COMMITTER_EMAIL 'josh.marsh@optum.com'
+end
