788447b2785c15a0449dd664de1effe8ef5e59fd
Joshua Marsh (icub3d) <joshua@themarshians.com>
2021-05-19 04:01:26 -0600

    fix new lines

--- packages/post-install/dev.fish
+++ packages/post-install/dev.fish
@@ -4,7 +4,9 @@
 if test ! -e ~/.cargo/bin/rustc
 	curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
 	set -x PATH $HOME/.cargo/bin $PATH
+	rustup toolchain add stable
 	rustup toolchain add nightly
+	rustup default stable
 	rustup component add rls rust-analysis rust-src
 else
 	rustup update
