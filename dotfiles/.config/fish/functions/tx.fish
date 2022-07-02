9a3d841b29ff6a9de04748c80fc9dd4cd22b3d94
Joshua Marsh (icub3d) <joshua@themarshians.com>
2022-07-02 16:50:22 -0600

    rearrange default windows

--- dotfiles/.config/fish/functions/tx.fish
+++ dotfiles/.config/fish/functions/tx.fish
@@ -1,23 +1,20 @@
 function tx
 	set -x SIMPLE_PROMPT true
 
-	# start emacs daemon
-	# em
+	command tmux new-session -d -c ~/dev/dotfiles -s dot -n dot
+
+	command tmux new-session -d -c ~ -s home -n "home"
+	command tmux new-window -d -c ~/dev -t home:1 -n "dev"
 
 	# our notes
 	set NOTES_DIR "$HOME/Documents/notes"
 	command tmux new-session -d -c "$NOTES_DIR" -s notes -n "notes"
 	
-	command tmux new-session -d -c ~ -s home -n "home"
-	command tmux new-session -d -c ~/dev/dotfiles -s dot -n dot
-	command tmux new-window -d -c ~/dev -t home:1 -n "dev"
-
 	if test "$ATWORK" = "true"
 		command tmux new-session -d -c ~/dev/oti-azure -s oti -n "code"
 		command tmux new-window -d -c ~/dev/oti-azure -t oti:1 -n "fish"
 		command tmux new-window -d -c ~/dev/oti-azure -t oti:2 -n "run"
 	else
-		command tmux new-window -d -c ~/dev -t home:1 -n "dev"
 		command tmux new-session -d -c ~ -s srv -n srv2
 	end
 	
