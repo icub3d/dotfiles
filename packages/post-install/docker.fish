120ee8b6cc61e2fa44ee1c974ab5f4fc7d4a2be1
Joshua Marsh (icub3d) <joshua@themarshians.com>
2022-07-23 10:36:14 -0600

    secretservices doesn't work on armv7l

--- packages/post-install/docker.fish
+++ packages/post-install/docker.fish
@@ -3,22 +3,8 @@
 add_group docker
 add_service docker
 
-# Ubuntu/WSL doesn't nee this.
-if test "$DISTO" != "Ubuntu"
-	set LATEST (github_latest docker docker-credential-helpers)
-	set VERSION ""
-	if command -vq docker-credential-secretservice
-		set VERSION (docker-credential-secretservice version)
-	end
-
-	if test "$LATEST" != "$VERSION" 
-		http -F "https://github.com/docker/docker-credential-helpers/releases/download/$LATEST/docker-credential-secretservice-$LATEST-amd64.tar.gz" > tmp.tar.gz
-		tar -xzf tmp.tar.gz
-		chmod a+x docker-credential-secretservice
-		mv docker-credential-secretservice ~/bin
-		rm tmp.tar.gz
-	end
-
+# Ubuntu/WSL and armv7 doesn't need this.
+if test "$DISTO" != "Ubuntu" -a (uname -m) = "x86_64"
 	if not test -f $HOME/.config/docker.json
 		echo '{"credsStore": "secretservice"}' >$HOME/.config/docker.json
 	end
