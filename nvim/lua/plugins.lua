12c2698ed720ab192625427097f001cb6d48cff2
Joshua Marsh (icub3d) <joshua@themarshians.com>
2023-03-07 15:08:22 -0700

    nvim stuff

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -266,6 +266,26 @@ local plugins = {
     },
     config = require('config.dap'),
   },
+
+  -- neorg
+  {
+    "nvim-neorg/neorg",
+    build = ":Neorg sync-parsers",
+    opts = {
+      load = {
+        ["core.defaults"] = {}, -- Loads default behaviour
+        ["core.norg.concealer"] = {}, -- Adds pretty icons to your documents
+        ["core.norg.dirman"] = { -- Manages Neorg workspaces
+          config = {
+            workspaces = {
+              notes = "~/Documents/notes",
+            },
+          },
+        },
+      },
+    },
+    dependencies = { { "nvim-lua/plenary.nvim" } },
+  }
 }
 
 require("lazy").setup(plugins)
