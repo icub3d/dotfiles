039cc9c7e772d9d38ddb78ecf069f0011abb1d7e
Joshua Marsh (icub3d) <joshua@themarshians.com>
2023-03-20 14:19:45 -0600

    nerd fonts changes

---
+++ dotfiles/.config/fish/functions/smoke-test-ligatures.fish
@@ -0,0 +1,9 @@
+function smoke-test-ligatures
+  echo "Normal"
+  echo -e "\033[1mBold\033[22m"
+  echo -e "\033[3mItalic\033[23m"
+  echo -e "\033[3;1mBold Italic\033[0m"
+  echo -e "\033[4mUnderline\033[24m"
+  echo -e "== === !== >= <= =>"
+  echo -e "契          勒 鈴 "
+end
