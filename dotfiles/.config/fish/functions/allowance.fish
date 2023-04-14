5f9bcf4c7428021e339405e75a3ccc45106e4999
Joshua Marsh (icub3d) <joshua@themarshians.com>
2023-04-14 14:26:20 -0600

    update helper functions

--- dotfiles/.config/fish/functions/allowance.fish
+++ dotfiles/.config/fish/functions/allowance.fish
@@ -1,5 +1,5 @@
 function allowance
-  bankctl -u (cat ~/Documents/ssssh/mongo-bank-uri) -d bank 1000 james.marshian@gmail.com
-  bankctl -u (cat ~/Documents/ssssh/mongo-bank-uri) -d bank 1000 william.marshian@gmail.com
-  bankctl -u (cat ~/Documents/ssssh/mongo-bank-uri) -d bank 1500 samuel.marshian@gmail.com
+  bankctl -u (cat ~/Documents/ssssh/mongo-bank-uri) -d bank add 1000 james.marshian@gmail.com allownace
+  bankctl -u (cat ~/Documents/ssssh/mongo-bank-uri) -d bank add 1000 william.marshian@gmail.com allowance
+  bankctl -u (cat ~/Documents/ssssh/mongo-bank-uri) -d bank add 1500 samuel.marshian@gmail.com allowance
 end
--- dotfiles/.config/fish/functions/ns.fish
+++ dotfiles/.config/fish/functions/ns.fish
@@ -1,3 +1,5 @@
 function ns
-	command tmux new-session -d -c $argv[2] -s $argv[1]
+	command tmux new-session -d -c $argv[2] -s $argv[1] -n v 
+  command tmux send-keys -t $argv[1]:0 "v ." C-m
+  command tmux new-window -d -t $argv[1] -n sh 
 end
