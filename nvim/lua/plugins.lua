888ac3f93b4d62e9e9f0c435a4172e9d3ac22a2c
Joshua Marsh (icub3d) <joshua@themarshians.com>
2023-05-01 10:12:29 -0600

    more desktop updates

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -315,24 +315,24 @@ local plugins = {
   },
 
   -- neorg
-  {
-    "nvim-neorg/neorg",
-    build = ":Neorg sync-parsers",
-    opts = {
-      load = {
-        ["core.defaults"] = {},       -- Loads default behaviour
-        ["core.norg.concealer"] = {}, -- Adds pretty icons to your documents
-        ["core.norg.dirman"] = {      -- Manages Neorg workspaces
-          config = {
-            workspaces = {
-              notes = "~/Documents/notes",
-            },
-          },
-        },
-      },
-    },
-    dependencies = { { "nvim-lua/plenary.nvim" } },
-  }
+  -- {
+  --   "nvim-neorg/neorg",
+  --   build = ":Neorg sync-parsers",
+  --   opts = {
+  --     load = {
+  --       ["core.defaults"] = {},       -- Loads default behaviour
+  --       ["core.norg.concealer"] = {}, -- Adds pretty icons to your documents
+  --       ["core.norg.dirman"] = {      -- Manages Neorg workspaces
+  --         config = {
+  --           workspaces = {
+  --             notes = "~/Documents/notes",
+  --           },
+  --         },
+  --       },
+  --     },
+  --   },
+  --   dependencies = { { "nvim-lua/plenary.nvim" } },
+  -- }
 }
 
 require("lazy").setup(plugins)
--- packages/post-install/nodejs.fish
+++ packages/post-install/nodejs.fish
@@ -4,4 +4,4 @@ if test ! -f ~/.npmrc
 	echo -en "prefix=/home/jmarsh/.npm-packages\n" >~/.npmrc
 end
 
-npm install --global yarn typescript-language-server pyright typescript yaml-language-server neovim emmet-ls browser-sync
+npm install --global yarn typescript-language-server pyright typescript yaml-language-server neovim emmet-ls browser-sync json-server
