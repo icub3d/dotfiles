92e9f9fff3cf8a8eb145626f568fdd00f4d587c3
Joshua Marsh (icub3d) <joshua@themarshians.com>
2023-06-01 19:53:32 -0600

    simplify ns

--- dotfiles/.config/fish/functions/get-marshian-images.fish
+++ dotfiles/.config/fish/functions/get-marshian-images.fish
@@ -1,11 +1,11 @@
 function get-marshian-images
     command mkdir -p ~/Pictures
-    for file in marshians-text-green-background-2k.png marshians-text-green-background-3k.png marshians-text-green-background-4k.png 
+    for file in marshians-text-green-background-2k.png marshians-text-green-background-3k.png marshians-text-green-background-4k.png marshians-text-green-400.png
         if test ! -f "$HOME/Pictures/$file"
             command http "https://logo.marsh.gg/dist/marshians-text-green/$file" > "$HOME/Pictures/$file"
         end
     end
-    for file in marshians-green-background-2k.png marshians-green-background-3k.png marshians-green-background-4k.png 
+    for file in marshians-green-background-2k.png marshians-green-background-3k.png marshians-green-background-4k.png marshians-green-400.png
         if test ! -f "$HOME/Pictures/$file"
             command http "https://logo.marsh.gg/dist/marshians-green/$file" > "$HOME/Pictures/$file"
         end
--- dotfiles/.config/fish/functions/ns.fish
+++ dotfiles/.config/fish/functions/ns.fish
@@ -1,4 +1,7 @@
 function ns
+  if test "$argv[2]" = ""
+    set argv $argv[1] (pwd)
+  end
 	command tmux new-session -d -c $argv[2] -s $argv[1] -n v 
   command tmux send-keys -t $argv[1]:0 "v ." C-m
   command tmux new-window -d -t $argv[1] -n sh -c $argv[2]
