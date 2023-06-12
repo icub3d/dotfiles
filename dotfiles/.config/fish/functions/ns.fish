4e04a7104789a12e0e3b14a1f6c54c3b61aa7677
Joshua Marsh (icub3d) <joshua@themarshians.com>
2023-06-12 17:13:33 -0600

    rename ns 0

--- dotfiles/.config/fish/functions/ns.fish
+++ dotfiles/.config/fish/functions/ns.fish
@@ -2,6 +2,6 @@ function ns
 	if test "$argv[2]" = ""
 		set argv $argv[1] (pwd)
 	end
-	command tmux new-session -d -c $argv[2] -s $argv[1] -n v 
+	command tmux new-session -d -c $argv[2] -s $argv[1] -n e
 	command tmux new-window -d -t $argv[1] -n sh -c $argv[2]
 end
