5f032677c75f7de432b5a539663f08f0dc6470ab
Joshua Marsh (icub3d) <joshua@themarshians.com>
2023-06-15 18:07:58 -0600

    emacs as editor

--- dotfiles/.config/fish/config.d/editor.fish
+++ dotfiles/.config/fish/config.d/editor.fish
@@ -2,11 +2,10 @@
 
 set -U ALTERNATE_EDITOR ""
 
-if command -vq nvim
+if command -vq emacs
+	set -x EDITOR "emacs -nw"
+else if command -vq nvim
 	set -x EDITOR "nvim"
-  else if command -vq emacs
-	set -x EDITOR "emacsclient -t"
-	set -x VISUAL "emacsclient -c -a emacs"
 else if command -vq code
 	set -x EDITOR "code --wait"
 end
--- dotfiles/.config/fish/functions/scan-help.fish
+++ dotfiles/.config/fish/functions/scan-help.fish
@@ -4,7 +4,7 @@ function scan-help
 	pushd $argv[1]
 	set files (/usr/bin/ls)
 	for file in $files
-		firefox-developer-edition $file >/dev/null 2>/dev/null &
+		open $file >/dev/null 2>/dev/null &
 		read -l -P "new name ($file): " name
 		if test -n "$name"
 			mv $file $name.pdf
