02f3092227e435a3b75057febf7da44972ca0a72
Joshua Marsh (icub3d) <joshua@themarshians.com>
2023-06-21 10:25:12 -0600

    find pod with kubectl command

--- dotfiles/.config/fish/functions/k.fish
+++ dotfiles/.config/fish/functions/k.fish
@@ -8,17 +8,22 @@ function k
 	else if test "$argv[1]" = "ns"
 		set argv config set-context --current --namespace=$argv[2]
 	else if test "$argv[1]" = "bash"
-		set argv exec -it $argv[2] -- /bin/bash
+		set POD (kubectl get po | cols 1 | rg $argv[2] | head -1)
+		set argv exec -it $POD -- /bin/bash
 	else if test "$argv[1]" = "sh"
-		set argv exec -it $argv[2] -- /bin/sh
+		set POD (kubectl get po | cols 1 | rg $argv[2] | head -1)
+		set argv exec -it $POD -- /bin/sh
 	else if test "$argv[1]" = "run"
-		set argv exec $argv[2] -- $argv[3..]
+		set POD (kubectl get po | cols 1 | rg $argv[2] | head -1)
+		set argv exec $POD -- $argv[3..]
 	else if test "$argv[1]" = "uc"
 		set argv config use-context $argv[2]
 	else if test "$argv[1]" = "ga"
 		set argv get all $argv[2..-1]
 	else if test "$argv[1]" = "oti"
 		set argv config use-context "aks-oti-$argv[2]-eastus-001"
+	else if test "$argv[1]" = "otvm"
+		set argv config use-context "otvm-containerized-$argv[2]"
 	end
 	command kubectl $argv
 end
