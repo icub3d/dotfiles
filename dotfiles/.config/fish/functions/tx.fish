b1bb41b766920f18f808b95a1aebe897f4e07992
Joshua Marsh (icub3d) <joshua@themarshians.com>
2023-09-10 12:14:01 -0600

    change editors

--- dotfiles/.config/fish/functions/tx.fish
+++ dotfiles/.config/fish/functions/tx.fish
@@ -1,27 +1,18 @@
 function tx
 	set -x SIMPLE_PROMPT true
 
-	command tmux new-session -d -c ~/dev/dotfiles -s • -n "•"
-	command tmux send-keys -t •:0 "emacs -nw ." C-m
-	command tmux new-window -d -c ~/dev/dotfiles/dotfiles/.config/emacs -t •:1 -n "👺"
-	command tmux send-keys -t •:1 "emacs --fg-daemon" C-m
-	command tmux new-window -d -c ~/dev/dotfiles -t •:2 -n "🐟"
-
+	command tmux new-session -d -c ~/dev/dotfiles -s • -n "🐟"
 
 	command tmux new-session -d -c ~ -s 🏠 -n "🏠"
 	command tmux new-window -d -c ~/dev -t 🏠:1 -n "🤖"
 	command tmux new-window -d -c ~ -t 🏠:2 -n "🏢"
 
 	if test "$ATWORK" = "true"
-		command tmux new-session -d -c ~/dev/oti-azure -s oti -n "🐧"
-		command tmux send-keys -t oti:0 "emacs -nw ." C-m
+		command tmux new-session -d -c ~/dev/oti-azure -s oti -n "🐟"
 		command tmux new-window -d -c ~/dev/oti-azure -t oti:1 -n "🏃"
-		command tmux new-window -d -c ~/dev/oti-azure -t oti:2 -n "🐟"
 
-		command tmux new-session -d -c ~/dev/edi-oti-otvm_containerized -s otvm -n "🐧"
-		command tmux send-keys -t otvm:0 "emacs -nw ." C-m
+		command tmux new-session -d -c ~/dev/edi-oti-otvm_containerized -s otvm -n "🐟"
 		command tmux new-window -d -c ~/dev/edi-oti-otvm_containerized -t otvm:1 -n "🏃"
-		command tmux new-window -d -c ~/dev/edi-oti-otvm_containerized -t otvm:2 -n "🐟"
 	end
 	
 	command tmux attach -t 🏠
