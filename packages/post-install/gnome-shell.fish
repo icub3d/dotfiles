555ad51b68bc0394eab0f4f6d981d1d88365f8ac
Joshua Marsh (icub3d) <joshua@themarshians.com>
2024-01-12 22:26:49 -0700

    gnome-shell

--- packages/post-install/gnome-shell.fish
+++ packages/post-install/gnome-shell.fish
@@ -11,8 +11,8 @@ gsettings set org.gnome.desktop.interface clock-show-seconds true
 gsettings set org.gnome.desktop.interface gtk-theme 'Adwaita-dark'
 gsettings set org.gnome.desktop.background show-desktop-icons 'false'
 gsettings set org.gnome.desktop.session idle-delay 0
-gsettings set org.gnome.desktop.background picture-uri 'file:///home/jmarsh/Pictures/marshians-text-green-background-4k.png'
-gsettings set org.gnome.desktop.screensaver picture-uri 'file:///home/jmarsh/Pictures/marshians-text-green-background-4k.png'
+gsettings set org.gnome.desktop.background picture-uri 'file:///home/jmarsh/Pictures/marshians-green-background-4k.png'
+gsettings set org.gnome.desktop.screensaver picture-uri 'file:///home/jmarsh/Pictures/marshians-green-background-4k.png'
 gsettings set org.gnome.settings-daemon.plugins.power sleep-inactive-ac-type 'nothing'
 gsettings set org.gnome.settings-daemon.plugins.power power-button-action  'suspend'
 
@@ -24,7 +24,7 @@ dconf write /org/gnome/shell/enabled-extensions "['desktop-icons@csoriano', 'tra
 dconf write /org/gnome/shell/extensions/desktop-icons/show-home false
 dconf write /org/gnome/shell/extensions/desktop-icons/show-trash false
 dconf write /org/gnome/shell/favorite-apps "['firefoxdeveloperedition.desktop', 'org.wezfurlong.wezterm.desktop', 'discord.desktop', 'com.obsproject.Studio.desktop', 'org.gnome.Nautilus.desktop', 'virt-manager.desktop', 'net.lutris.Lutris.desktop', 'steam-native.desktop', 'minecraft-launcher.desktop']"
-dconf write /org/gnome/shell/extensions/user-theme/name 'marshians'
+# dconf write /org/gnome/shell/extensions/user-theme/name 'marshians'
 dconf write /org/gnome/mutter/keybindings/switch-monitor '@as []'
 
 # stylus
